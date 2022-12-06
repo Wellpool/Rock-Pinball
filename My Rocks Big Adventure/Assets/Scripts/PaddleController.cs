cd3503d343671c62f4842b24ff36384074a73bec
Peter18281 <Peter18281@gmail.com>
2022-12-06 17:38:03 +0000

    Paddle Input Fix

--- My Rocks Big Adventure/Assets/Scripts/PaddleController.cs
+++ My Rocks Big Adventure/Assets/Scripts/PaddleController.cs
@@ -7,7 +7,7 @@ public class PaddleController : MonoBehaviour
     public bool RightPaddle;
     //Simple script to use the motor on each paddle based on which key is pressed.
     void Update(){
-        if((RightPaddle && Input.GetKey(KeyCode.A)) || (!RightPaddle && Input.GetKey(KeyCode.D))){
+        if((RightPaddle && (Input.GetAxis("Horizontal") > 0)) || (!RightPaddle && (Input.GetAxis("Horizontal") < 0))){
             GetComponent<HingeJoint2D>().useMotor = true;
         }
         else{
